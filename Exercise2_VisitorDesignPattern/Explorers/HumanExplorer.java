d9b131d4d2c28b778ce906a0f6aaf6221c7da4f9
Katherine Espinosa <katherinemaegespinosa@gmail.com>
2023-05-02 11:01:56 +0800

    Add files via upload

---
+++ Exercise2_VisitorDesignPattern/Explorers/HumanExplorer.java
@@ -0,0 +1,32 @@
+package Explorers;
+
+import Planets.*;
+
+public class HumanExplorer implements Explorer {
+    @Override
+    public void visit(Mercury mercury) {
+        System.out.println("Deploying tools specific to Mercury... exploring life");
+    }
+
+    @Override
+    public void visit(Mars mars) {
+        System.out.println("Deploying tools specific to Mars... exploring life");
+    }
+
+    @Override
+    public void visit(Saturn saturn) {
+        System.out.println("Deploying tools specific to Saturn... exploring life");
+    }
+
+
+    @Override
+    public void visit(Eris eris) {
+        System.out.println("Deploying tools specific to Eris... exploring life");
+    }
+
+    
+    @Override
+    public void visit(Ceres ceres) {
+        System.out.println("Deploying tools specific to Ceres... exploring life");
+    }
+}
