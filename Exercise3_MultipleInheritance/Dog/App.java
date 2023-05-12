0cf9373ac46008699ac116f5e88e5d944a27e32c
Katherine Espinosa <katherinemaegespinosa@gmail.com>
2023-05-12 12:00:29 +0800

    Create App.java

---
+++ Exercise3_MultipleInheritance/Dog/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Create object from pet class
+        MaltTzu myPet = new MaltTzu();
+
+        myPet.bark();
+        System.out.println("Eye Color: "+ myPet.eyeColor);
+        System.out.println("Height: "+ myPet.height);
+    }
+}
